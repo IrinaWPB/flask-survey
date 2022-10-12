f9969c921b5c24b4fe7baffae7cd73b6fba3acf4
IrinaJ <irina.ul87@gmail.com>
2022-10-12 11:24:17 -0400

    commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from urllib import response
-from flask import Flask, render_template, redirect, request, flash
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -8,17 +8,21 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 debug = DebugToolbarExtension
 
-responses = []
 
 @app.route('/')
 def start_page():
     """Shows title of th survey with instructions"""
-
+    
     title = satisfaction_survey.title
     instructions = satisfaction_survey.instructions
     return render_template('index.html', 
         title = title, 
         instructions = instructions)
+
+@app.route('/start_session', methods=["POST"])
+def start_session():
+    session['responses'] = []
+    return redirect('/questions/0')
 
 @app.route('/questions/<int:quest_id>')
 def question(quest_id):
@@ -26,12 +30,12 @@ def question(quest_id):
 
     number_of_questions = len(satisfaction_survey.questions)
 
-    if len(responses) == number_of_questions:
+    if len(session['responses']) == number_of_questions:
         flash('You already answered all the questions')
         return redirect('/thankyou')
 
-    if quest_id > len(responses) or quest_id >= number_of_questions:
-        quest_id = len(responses)
+    if quest_id > len(session['responses']) or quest_id >= number_of_questions:
+        quest_id = len(session['responses'])
         flash('You are trying to visit invalid page. Please answer the question')
         return redirect(f'/questions/{quest_id}')
 
@@ -45,11 +49,12 @@ def question(quest_id):
 @app.route('/answer', methods=["POST"])
 def answer():
     """Add user's answers to the responses list"""
-
     answer = request.form['choice']
     quest_id = int(request.form['quest_id'])
+    responses = session['responses']
     responses.append(answer)
-    if len(responses) == len(satisfaction_survey.questions):
+    session['responses'] = responses
+    if len(session['responses']) == len(satisfaction_survey.questions):
         return redirect('/thankyou')
     return redirect(f'/questions/{quest_id+1}')
 
